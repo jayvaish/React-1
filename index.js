79d435401cbd5c302228734ecd03837276d4a455
Ajay Thakur <103938110+jayvaish@users.noreply.github.com>
2022-09-13 08:03:42 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+import React from "react";
+import ReactDOM  from "react-dom";
+
+ReactDOM.render(
+    <>
+<h1> Technical Skill</h1>
+<p1>5 neflix seris</p1>
+<ol>
+    <li>Dark</li>
+    <li>my hole love</li>
+
+</ol>
+</>,
+document
+);
